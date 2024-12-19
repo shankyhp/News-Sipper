ac6a5f4a326ad8e2c6b0eb2dc2b6e510ac37b8db
shankyhp <birajdarhpshankar@gmail.com>
2024-12-19 10:52:43 +0530

    news sipper completed

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
+
+const App = () => {
+
+  const pageSize = 5;
+
+
+  const [progress, setProgress] = useState(0)
+
+
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+
+        <LoadingBar height={3} color="#f11946" progress={progress} />
+
+
+        <Routes>
+
+          <Route path="/" element={<Navigate to="/general" />} />
+          <Route path="/general" element={<News setProgress={setProgress} pageSize={pageSize} key="general" country="us" category="general" />} />
+          <Route path="/business" element={<News setProgress={setProgress} pageSize={pageSize} key="business" country="us" category="business" />} />
+          <Route path="/health" element={<News setProgress={setProgress} pageSize={pageSize} key="health" country="us" category="health" />} />
+          <Route path="/entertainment" element={<News setProgress={setProgress} pageSize={pageSize} key="entertainment" country="us" category="entertainment" />} />
+          <Route path="/science" element={<News setProgress={setProgress} pageSize={pageSize} key="science" country="us" category="science" />} />
+          <Route path="/sports" element={<News setProgress={setProgress} pageSize={pageSize} key="technology" country="us" category="technology" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App
+
+
+
+
+
+
+
