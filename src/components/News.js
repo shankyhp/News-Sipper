3f1a467419130285a47de2e7284aa54c31540f3e
shankyhp <birajdarhpshankar@gmail.com>
2024-12-20 13:59:14 +0530

    change in News.js

--- src/components/News.js
+++ src/components/News.js
@@ -32,7 +32,7 @@ const News = (props) => {
   }
 
   useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+    document.title = `${capitalizeFirstLetter(props.category)} - News-Sipper`;
     updateNews();
     // eslint-disable-next-line
   }, [])
@@ -49,7 +49,7 @@ const News = (props) => {
 
   return (
     <>
-      <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+      <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>News-Sipper - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
       {loading && <Spinner />}
       <InfiniteScroll
         dataLength={articles.length}
